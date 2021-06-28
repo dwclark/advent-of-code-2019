9fdea22f0daac48d3e6bf790389e1248d7842eee
David Clark <david.clark@moogsoft.com>
2021-06-28 15:14:46 -0700

    Final cleanup for day 12

--- day12.groovy
+++ day12.groovy
@@ -2,82 +2,65 @@ import groovy.transform.ToString
 import groovy.transform.CompileStatic
 import static java.lang.Math.abs
 
-Position parsePosition(String line) {
+int[] parsePosition(String line) {
     def re = /<x=(-*[0-9]+), y=(-*[0-9]+), z=(-*[0-9]+)>/
     def m = line =~ re
-    return new Position(m[0][1].toInteger(), m[0][2].toInteger(), m[0][3].toInteger())
+    return [ m[0][1].toInteger(), m[0][2].toInteger(), m[0][3].toInteger() ] as int[]
 }
 
-@CompileStatic
-class Position {
-    int x, y, z
-
-    Position(int x, int y, int z) { this.x = x; this.y = y; this.z = z }
+@ToString @CompileStatic
+class Moon {
+    static int X = 0, Y = 1, Z = 2
+    int id
+    int[] position
+    int[] velocity
     
-    void add(Velocity v) {
-        x += v.x
-        y += v.y
-        z += v.z
-    }
+    public Moon(int i, int[] p, int[] v) { id = i; position = p; velocity = v; }
     
-    int energy() { return abs(x) + abs(y) + abs(z) }
-
-    Position copy() { return new Position(x, y, z) }
-}
-
-@CompileStatic
-class Velocity {
-    int x, y, z
-
-    Velocity(int x, int y, int z) { this.x = x; this.y = y; this.z = z }
-
     int changeBy(int me, int them) {
         if(me < them) return 1
         else if(me > them) return -1
         else return 0
     }
     
-    void add(Position me, Position other) {
-        x += changeBy(me.x, other.x)
-        y += changeBy(me.y, other.y)
-        z += changeBy(me.z, other.z)
+    void add(int[] other) {
+        velocity[X] += changeBy(position[X], other[X])
+        velocity[Y] += changeBy(position[Y], other[Y])
+        velocity[Z] += changeBy(position[Z], other[Z])
     }
-
-    int energy() { abs(x) + abs(y) + abs(z) }
-
-    Velocity copy() { new Velocity(x, y, z) }
-}
-
-@ToString @CompileStatic
-class Moon {
-    int id
-    Position position
-    Velocity velocity
-    
-    public Moon(int i, Position p, Velocity v) { id = i; position = p; velocity = v; }
 
     public void nextVelocities(List<Moon> moons) {
         for(int i = 0; i < moons.size(); ++i) {
             Moon m = moons.get(i);
             if(m.id != id) {
-                velocity.add(position, m.position)
+                add(m.position)
             }
         }
     }
 
     public void nextPosition() {
-        position.add(velocity)
+        for(int i = X; i <= Z; ++i) position[i] += velocity[i]
     }
 
     public Moon copy() {
-        return new Moon(id, position.copy(), velocity.copy())
+        return new Moon(id, Arrays.copyOf(position, position.length), Arrays.copyOf(velocity, velocity.length))
     }
+
+    int energy(int[] ary) { return abs(ary[X]) + abs(ary[Y]) + abs(ary[Z]) }
     
-    int energy() { position.energy() * velocity.energy() }
+    int energy() { return energy(position) * energy(velocity) }
+
+    static void runStep(List<Moon> moons) {
+        for(Moon m in moons) m.nextVelocities(moons)
+        for(Moon m in moons) m.nextPosition()
+    }
+
+    static int totalEnergy(List<Moon> moons) { (int) moons.sum { m -> m.energy() } }
 }
 
 @CompileStatic
 class DimensionStates {
+    static int X = 0, Y = 1, Z = 2
     int[] xstates = new int[8]
     int[] ystates = new int[8]
     int[] zstates = new int[8]
@@ -87,28 +70,10 @@ class DimensionStates {
 
     boolean getPeriodSolved() { return xperiod != 0 && yperiod != 0 && zperiod != 0 }
     
-    int[] fillX(int[] ary) {
+    int[] fill(int dimension, int[] ary) {
         for(int i = 0; i < moons.size(); ++i) {
-            ary[2 * i] = moons[i].position.x
-            ary[2*i + 1] = moons[i].velocity.x
-        }
-
-        return ary
-    }
-
-    int[] fillY(int[] ary) {
-        for(int i = 0; i < moons.size(); ++i) {
-            ary[2 * i] = moons[i].position.y
-            ary[2*i + 1] = moons[i].velocity.y
-        }
-
-        return ary
-    }
-
-    int[] fillZ(int[] ary) {
-        for(int i = 0; i < moons.size(); ++i) {
-            ary[2 * i] = moons[i].position.z
-            ary[2*i + 1] = moons[i].velocity.z
+            ary[2 * i] = moons[i].position[dimension]
+            ary[2*i + 1] = moons[i].velocity[dimension]
         }
 
         return ary
@@ -116,9 +81,9 @@ class DimensionStates {
     
     DimensionStates(List<Moon> moons) {
         this.moons = moons
-        fillX(xstates)
-        fillY(ystates)
-        fillZ(zstates)
+        fill(X, xstates)
+        fill(Y, ystates)
+        fill(Z, zstates)
     }
 
     BigInteger lcm() {
@@ -129,17 +94,16 @@ class DimensionStates {
     BigInteger repeatsEvery() {
         long steps = 0
         while(!periodSolved) {
-            for(Moon m in moons) m.nextVelocities(moons)
-            for(Moon m in moons) m.nextPosition()
+            Moon.runStep(moons)
             ++steps
             
-            if(xperiod == 0G && Arrays.equals(xstates, fillX(test)))
+            if(xperiod == 0G && Arrays.equals(xstates, fill(X, test)))
                 xperiod = steps
 
-            if(yperiod == 0G && Arrays.equals(ystates, fillY(test)))
+            if(yperiod == 0G && Arrays.equals(ystates, fill(Y, test)))
                 yperiod = steps
 
-            if(zperiod == 0G && Arrays.equals(zstates, fillZ(test)))
+            if(zperiod == 0G && Arrays.equals(zstates, fill(Z, test)))
                 zperiod = steps
         }
 
@@ -147,21 +111,12 @@ class DimensionStates {
     }
 }
 
-int totalEnergy(List<Moon> moons) {
-    moons.sum { m -> m.energy() }
-}
+
 
 int idx = 1
-List<Moon> initial = new File("data/12").readLines().collect { new Moon(idx++, parsePosition(it), new Velocity(0,0,0)) }.asImmutable()
+List<Moon> initial = new File("data/12").readLines().collect { new Moon(idx++, parsePosition(it), [0,0,0] as int[]) }.asImmutable()
 List<Moon> current = initial.collect { m -> m.copy() }
 
-1000.times {
-    for(Moon m in current)
-        m.nextVelocities(current)
-
-    for(Moon m in current)
-        m.nextPosition()
-}
-
+1000.times { Moon.runStep(current) }
 def states = new DimensionStates(initial.collect { m -> m.copy() })
-println "1: ${totalEnergy(current)}, 2: ${states.repeatsEvery()}"
+println "1: ${Moon.totalEnergy(current)}, 2: ${states.repeatsEvery()}"
