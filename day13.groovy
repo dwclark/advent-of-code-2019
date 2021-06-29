37b0f33ef319f48042ef6e3ad2b53635b41e5c48
David Clark <david.clark@moogsoft.com>
2021-06-29 11:57:05 -0700

    Finished day 13.
    Pass true to part 2 to show game in action

--- day13.groovy
+++ day13.groovy
@@ -1,8 +1,10 @@
 import groovy.transform.Immutable
 
 enum Tile {
-    EMPTY, WALL, BLOCK, PADDLE, BALL
+    EMPTY('.'), WALL('|'), BLOCK('B'), PADDLE('_'), BALL('*')
 
+    private Tile(String d) { display = d }
+    
     static Tile from(int val) {
         if(val == 0) return EMPTY
         else if(val == 1) return WALL
@@ -12,7 +14,7 @@ enum Tile {
         else throw new IllegalArgumentException()
     }
     
-    final int val
+    final String display
 }
 
 @Immutable
@@ -20,27 +22,110 @@ class Position {
     int left, top
 }
 
-def grid = [:]
-def ioBus = IoBus.separateChannels()
-def intCode = Intcode.from(new File("data/13"), ioBus)
-
-def robotThread = Thread.start {
-    intCode.call()
-    ioBus.writeChannel.put(-1)
-}
-
-def controlThread = Thread.start {
-    while(true) {
-        int xpos = ioBus.writeChannel.take()
-        if(xpos == -1)
-            break
-        
-        int ypos = ioBus.writeChannel.take()
-        Tile tile = Tile.from((int) ioBus.writeChannel.take())
-        grid[new Position(xpos, ypos)] = tile
+void drawGrid(Map grid) {
+    def lastPos = new Position(-1,-1)
+    grid.each { k, v ->
+        if(k.top != lastPos.top) println()
+        print v.display
+        lastPos = k
     }
+
+    println()
 }
 
-robotThread.join()
-controlThread.join()
-println "1: ${grid.values().count { t -> t == Tile.BLOCK }}"
+int part1() {
+    def grid = [:]
+    def ioBus = IoBus.separateChannels()
+    def intCode = Intcode.from(new File("data/13"), ioBus)
+
+    def controlThread = Thread.start {
+        try {
+            while(true) {
+                int xpos = ioBus.writeChannel.take()
+                if(xpos == -1)
+                    break
+                
+                int ypos = ioBus.writeChannel.take()
+                Tile tile = Tile.from((int) ioBus.writeChannel.take())
+                grid[new Position(xpos, ypos)] = tile
+            }
+        }
+        catch(InterruptedException ie) {}
+    }
+    
+    def robotThread = Thread.start {
+        intCode.call()
+        controlThread.interrupt()
+    }
+
+    robotThread.join()
+    controlThread.join()
+    return grid.values().count { t -> t == Tile.BLOCK }
+}
+
+int part2(boolean show = false) {
+    def grid = [:]
+    def ioBus = IoBus.separateChannels()
+    def intCode = Intcode.from(new File("data/13"), ioBus).store(0, 2L)
+    def scanner = new Scanner(System.in)
+    def paddleAt = new Position(0,0), ballAt = new Position(0,0)
+    int score;
+
+    def updateState = { initializing ->
+        while(true) {
+            int ins1 = ioBus.writeChannel.take()
+            int ins2 = ioBus.writeChannel.take()
+            int ins3 = ioBus.writeChannel.take()
+            if(ins1 == -1 && ins2 == 0) {
+                score = ins3
+                if(initializing)
+                    break
+            }
+            else {
+                Tile tile = Tile.from(ins3)
+                Position pos = new Position(ins1, ins2)
+                if(tile == Tile.BALL) ballAt = pos
+                if(tile == Tile.PADDLE) paddleAt = pos
+                grid[pos] = tile
+
+                if(!initializing && tile == Tile.BALL)
+                    break
+            }
+        }
+
+        if(show) {
+            println "score: $score"
+            drawGrid(grid)
+        }
+    }
+
+    def takeAction = {
+        if(paddleAt.left < ballAt.left) ioBus.readChannel.put(1L)
+        else if(paddleAt.left > ballAt.left) ioBus.readChannel.put(-1L)
+        else ioBus.readChannel.put(0L)
+    }
+
+    def controlThread = Thread.start {
+        try {
+            updateState(true)
+            while(true) {
+                takeAction()
+                updateState()
+            }
+        }
+        catch(InterruptedException ie) { }
+    }
+
+    def robotThread = Thread.start {
+        intCode.call()
+        controlThread.interrupt()
+    }
+        
+    robotThread.join()
+    controlThread.join()
+    return score
+}
+
+def p1 = part1()
+def p2 = part2()
+println "1: ${p1} 2: ${p2}"
