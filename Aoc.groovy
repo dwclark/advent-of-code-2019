f464482447d48f115148510afa53195f983c8a5f
David Clark <me@nobody.com>
2023-11-27 19:58:51 -0600

    Moving awk -> awk only folder
    Also starting to add some better utilities for groovy

---
+++ Aoc.groovy
@@ -0,0 +1,26 @@
+class Aoc {
+    
+    static void printAssert(Object... args) {
+	List grouped = (args as List).collate(3)
+	String toPrint = ""
+	grouped.each { sub -> 
+	    assert sub[1] == sub[2]
+	    toPrint += "${sub[0]} ${sub[1]} "
+	}
+
+	println toPrint.trim()
+    }
+
+    static List<String> lines(String path, Closure transform = { -> it }) {
+	new File(path).readLines().collect(transform)
+    }
+
+    static void main(String[] args) {
+	def shell = new GroovyShell()
+	(1..1).each { n ->
+	    String name = "day${n.toString().padLeft(2, '0')}.groovy"
+	    print "[Day ${n}] "
+	    shell.evaluate(new File(name))
+	}
+    }
+}
--- day01.groovy
+++ day01.groovy
@@ -1,5 +1,8 @@
-static single(w) { ((int) (w/3)) - 2; }
-static cont(f) { return single(f) <= 0 ? 0 : single(f) + cont(single(f)) }
-def lines = new File("data/01").readLines().collect { it.toInteger() }
-println "1: ${lines.sum { single(it) }}, 2: ${lines.sum { cont(it) }}"
-                              
+import static Aoc.*
+
+def all = lines('data/01') { it.toInteger() }
+def single(w) { w.intdiv(3) - 2 }
+def cont(f) { return single(f) <= 0 ? 0 : single(f) + cont(single(f)) }
+
+printAssert("Part 1:", all.sum { single(it) }, 3301059,
+	    "Part 2:", all.sum { cont(it) }, 4948732)
