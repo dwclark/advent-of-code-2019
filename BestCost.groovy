3cbab3988c299a589d2c064ee659d81069d1245e
David Clark <me@nobody.com>
2023-11-28 22:27:40 -0600

    Added best cost tracker

---
+++ BestCost.groovy
@@ -0,0 +1,37 @@
+class BestCost<D,C extends Comparable<C>> {
+    final Map<D,Map.Entry<D,C>> soFar = new HashMap<>()
+    final Heap<D,C> heap
+
+    BestCost(Heap<D,C> heap) {
+	this.heap = heap
+    }
+
+    D next() {
+	return heap.next()
+    }
+
+    void add(D d, C c) {
+	final Map.Entry<D,C> entry = soFar[d]
+	if(!entry) {
+	    soFar[d] = heap.insert(d, c)
+	}
+	else if(heap.cmp.compare(c, entry.value) == -1) {
+	    heap.extremize(entry, c)
+	}
+    }
+
+    static void main(String[] args) {
+	new BestCost<>(Heap.min()).with {
+	    add "one", 100
+	    add "two", 200
+	    add "three", 300
+	    add "three", 700
+	    add "three", 30
+
+	    assert next() == "three"
+	    assert next() == "one"
+	    assert next() == "two"
+	    assert !next()
+	}
+    }
+}
