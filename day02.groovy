bf73ea61894d27a4eca2c928b2b0ae2f0d9da913
David Clark <me@nobody.com>
2023-11-29 19:45:12 -0600

    Simplified, remembered how findResult works

--- day02.groovy
+++ day02.groovy
@@ -1,21 +1,15 @@
 import static Aoc.*
 
-def part1(Intcode vm) {
-    vm.store(1, 12).store(2, 2).call().load(0);
+def run(Intcode vm, int noun, int verb) {
+    vm.reset().store(1, noun).store(2, verb).call().load(0);
 }
 
 def part2(Intcode vm) {
-    for(int noun in (0..99)) {
-        for(int verb in (0..99)) {
-            if(19690720 == vm.reset().store(1, noun).store(2, verb).call().load(0)) {
-               return (100 * noun + verb)
-            }
-        }
-    }
+    (0..99).findResult { noun -> (0..99).findResult { verb -> 19690720 == run(vm, noun, verb) ? (100 * noun + verb) : null }}
 }
 
 def vm = Intcode.from(new File("data/02"))
-printAssert("Part 1:", part1(vm), 3409710,
+printAssert("Part 1:", run(vm, 12, 2), 3409710,
 	    "Part 2:", part2(vm), 7912)
 
 
