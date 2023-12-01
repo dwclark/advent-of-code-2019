5e01c6946354300ba05049fd860de606df6268e8
David Clark <me@nobody.com>
2023-11-30 18:48:36 -0600

    Refactor day 05

--- day05.groovy
+++ day05.groovy
@@ -1,9 +1,11 @@
+import static Aoc.*
+
 def bus = new IoBus().write(1)
 def vm = Intcode.from(new File("data/05"), bus).call()
-def v1 = vm.bus.lastWrite()
+def p1 = vm.bus.lastWrite()
 
 vm = vm.reset()
 vm.bus.write(5)
-v2 = vm.call().bus.lastWrite()
+p2 = vm.call().bus.lastWrite()
 
-println "1: ${v1}, 2: ${v2}"
+printAssert("Part 1:", p1, 13547311, "Part 2:", p2, 236453)
