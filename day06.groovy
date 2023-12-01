3481126a60cb3e6b9c25b76b2f821b70674a64f8
David Clark <me@nobody.com>
2023-12-01 16:56:59 -0600

    Finally made the code a bit shorter

--- day06.groovy
+++ day06.groovy
@@ -1,27 +1,16 @@
 import static Aoc.*
 
-Map makeOrbits(List lines) {
-    lines.inject([:]) { map, line ->
+List make(List lines) {
+    def orbits = [:], backwards = [:], paths = [:]
+    lines.each { line ->
 	(first, second) = line.split('\\)')
-	map.get(first, []) << second
-	map
+	orbits.get(first, []) << second
+	paths.get(first, []) << second
+	paths.get(second, []) << first
+	backwards[second] = first
     }
-}
 
-Map makeBackwards(Map orbits) {
-    orbits.inject([:]) { map, k, list ->
-	list.each { v -> map[v] = k }
-	map
-    }
-}
-
-Map makePaths(Map orbits, Map backwards) {
-    def copy = orbits.collectEntries { k, list -> new MapEntry(k, new ArrayList(list)) }
-    backwards.inject(copy) { m, k, v ->
-	if(m[k]) m[k].add(v)
-	else m[k] = [v]
-	m
-    }
+    [ orbits, backwards, paths ]
 }
 
 int findPathLength(Map paths) {
@@ -29,20 +18,13 @@ int findPathLength(Map paths) {
     bc.add('YOU', 0)
     String n = null
     while((n = bc.next()) != null) {
-	if(n == 'SAN') {
-	    return bc.cost(n) - 2
-	}
-	else {
-	    paths[n].each { p -> bc.add(p, bc.cost(n) + 1) }
-	}
+	if(n == 'SAN') return bc.cost(n) - 2
+	else paths[n].each { p -> bc.add(p, bc.cost(n) + 1) }
     }
 }
 
-orbits = makeOrbits(lines("data/06"))
-backwards = makeBackwards(orbits)
-paths = makePaths(orbits, backwards)
+(orbits, backwards, paths) = make(lines("data/06"))
 int countHops(String key) { key == "COM" ? 0 : 1 + countHops(backwards[key]) }
 int countOrbits() { backwards.keySet().sum { countHops(it) } }
 
-printAssert("Part 1:", countOrbits(), 147807,
-	    "Part 2:", findPathLength(paths), 229)
+printAssert("Part 1:", countOrbits(), 147807, "Part 2:", findPathLength(paths), 229)
