b778a8b7b24056741fbe28259d84839d9b237891
David Clark <me@nobody.com>
2023-12-01 17:32:58 -0600

    Removed obvious duplications

--- day07.groovy
+++ day07.groovy
@@ -1,58 +1,32 @@
+import static Aoc.*
 import java.util.concurrent.LinkedBlockingQueue;
 
-def phase1AmpsInit = { code ->
-    def queues = (0..5).collect { new LinkedBlockingQueue<>() }
-    def ios = (1..5).collect { i -> new IoBus(queues[i-1], queues[i]) }
-    def amps = (0..<5).collect { i -> Intcode.from(code, ios[i]) }
-    return amps
+List init(File code, Range qrange, Range iosRange, Range ampsRange) {
+    def queues = qrange.collect { new LinkedBlockingQueue<>() }
+    def ios = iosRange.collect { i -> new IoBus(queues[i-1], queues[i]) }
+    ampsRange.collect { i -> Intcode.from(code, ios[i]) }
 }
 
-def executePhase1 = { amps, List<Integer> phases ->
-    amps.each {
-        it.reset();
-        it.bus.reset() }
+def execute = { boolean threaded, List amps, List<Integer> phases ->
+    amps.each { it.reset(); it.bus.reset() }
     amps.eachWithIndex { amp, i ->
         if(i == 0) amp.bus.seedRead(phases[i], 0)
         else amp.bus.seedRead(phases[i])
     }
 
-    amps.each { it.call(); }
+    if(threaded)
+	amps.collect { amp -> Thread.start { amp.call() } }.each { it.join() }
+    else 
+	amps.each { it.call(); }
+
     return amps[4].bus.lastWrite()
 }
 
-def phase2AmpsInit = { code ->
-    def queues = (0..<5).collect { new LinkedBlockingQueue<>() }
-    def ios = (0..<5).collect { i -> new IoBus(queues[i-1], queues[i]) }
-    def amps = (0..<5).collect { i -> Intcode.from(code, ios[i]) }
-    return amps
+int findMax(List<Integer> phases, List amps, Closure toExec) {
+    phases.permutations { perm -> toExec.call(amps, perm) }.max()
 }
 
-def executePhase2 = { amps, List<Integer> phases ->
-    amps.each {
-        it.reset();
-        it.bus.reset() }
-    amps.eachWithIndex { amp, i ->
-        if(i == 0) amp.bus.seedRead(phases[i], 0)
-        else amp.bus.seedRead(phases[i])
-    }
-
-    def threads = amps.collect { amp -> Thread.start { amp.call() } }
-    threads.each { it.join() }
-    return amps[4].bus.lastWrite()
-}
-
-def day7 = { phases, amps, closure ->
-    def thruster = -1
-    
-    phases.eachPermutation { phase ->
-        def tmp = closure.call(amps, phase)
-        thruster = Math.max(thruster, tmp)
-    }
-
-    return thruster
-}
-
-def p1 = day7([0,1,2,3,4], phase1AmpsInit(new File("data/07")), executePhase1)
-def p2 = day7([5,6,7,8,9], phase2AmpsInit(new File("data/07")), executePhase2)
-
-println "1: ${p1}, 2: ${p2}"
+File file = new File("data/07")
+def p1 = findMax([0,1,2,3,4], init(file, (0..5), (1..5), (0..<5)), execute.curry(false))
+def p2 = findMax([5,6,7,8,9], init(file, (0..<5), (0..<5), (0..<5)), execute.curry(true))
+printAssert("Part 1:", p1, 359142, "Part 2:", p2, 4374895)
