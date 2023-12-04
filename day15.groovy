24173eb36e3b809c4c45ba87abe8b5eb67b4799e
David Clark <me@nobody.com>
2023-12-03 21:11:50 -0600

    Finally got day 15 working again

--- day15.groovy
+++ day15.groovy
@@ -1,109 +1,110 @@
+import static Aoc.*
 import groovy.transform.Immutable
+import groovy.transform.ToString
 
-enum Status {
-    WALL(0L), MOVED(1L), OXYGEN(2L);
-    private Status(long val) { this.val = val }
+enum Content {
+    WALL(0L), FREE(1L), OXYGEN(2L);
+    private Content(long val) { this.val = val }
     final long val;
-
-    static Status from(long val) {
-        if(val == WALL.val) return WALL
-        else if(val == MOVED.val) return MOVED
-        else if(val == OXYGEN.val) return OXYGEN
-        else throw new IllegalArgumentException()
-    }
+    private static final Map<Long,Content> _from = [ (0L): WALL, (1L): FREE, (2L): OXYGEN ]
+    static Content from(long val) { _from[val] }
 }
 
 enum Direction {
     NORTH(1L), SOUTH(2L), WEST(3L), EAST(4L);
     private Direction(long val) { this.val = val }
     final long val
-
-    public Direction getUndo() {
-        if(this == NORTH) return SOUTH
-        else if(this == SOUTH) return NORTH
-        else if(this == EAST) return WEST
-        else return EAST
-    }
+    private static final Map<Direction,Direction> _back = [ (NORTH): SOUTH, (SOUTH): NORTH, (EAST): WEST, (WEST): EAST ]
+    Direction getBack() { _back[this] }
+    static final List<Direction> all = [ NORTH, SOUTH, EAST, WEST ].asImmutable()
 }
 
 @Immutable class Position {
     int x, y
 
-    @Override String toString() { "($x,$y)" }
-
-    Position move(Direction d) {
-        if(d == Direction.NORTH) return new Position(x, y+1)
-        else if(d == Direction.SOUTH) return new Position(x, y-1)
-        else if(d == Direction.WEST) return new Position(x-1, y)
-        else return new Position(x+1, y)
-    }
-
-    Direction move(Position newPosition) {
-        if(newPosition.y > y) return Direction.NORTH
-        else if(newPosition.y < y) return Direction.SOUTH
-        else if(newPosition.x > x) return Direction.EAST
-        else return Direction.WEST
+    Position plus(Direction d) {
+	switch(d) {
+	    case Direction.NORTH: return new Position(x, y+1)
+	    case Direction.SOUTH: return new Position(x, y-1)
+	    case Direction.WEST: return new Position(x-1, y)
+	    default: return new Position(x+1, y)
+	}
     }
 }
 
-class DiscoveringDfsWalk extends Graph.DfsWalk<Position> {
-    Position oxygen
-    IoBus ioBus
-    Graph graph
+@ToString
+class Tries {
+    final Position position
+    final Direction direction
+    Content content
+    final Set nextDirections
 
-    void discover(final Position current, final Direction d) {
-        ioBus.readChannel.put(d.val)
-        Status response = Status.from(ioBus.writeChannel.take())
-        if(response == Status.WALL) return
-
-        if(response == Status.MOVED || response == Status.OXYGEN) {
-            Position p = current.move(d)
-            graph.addNode(p)
-            graph.addEdge(current, p)
-            if(response == Status.OXYGEN) oxygen = p
-            ioBus.readChannel.put(d.undo.val)
-            Status undoResponse = Status.from(ioBus.writeChannel.take())
-            assert undoResponse != Status.WALL
-        }
+    Tries(Position position) {
+	this.position = position
+	this.direction = null
+	this.content = Content.FREE
+	this.nextDirections = new HashSet(Direction.all)
     }
     
-    @Override
-    Graph.DfsStep<Position> startVisit(final Position current, final Position predecessor) {
-        //println "starting visit at $current"
-        if(predecessor != null) {
-            //need to actually move to what graph thinks is current
-            Direction d = predecessor.move(current)
-            ioBus.readChannel.put(d.val)
-            Status response = Status.from(ioBus.writeChannel.take())
-            assert response != Status.WALL
-        }
-        
-        discover(current, Direction.NORTH)
-        discover(current, Direction.SOUTH)
-        discover(current, Direction.EAST)
-        discover(current, Direction.WEST)
-        return super.startVisit(current, predecessor)
+    Tries(Position from, Direction direction) {
+	this.position = from + direction
+	this.direction = direction
+	this.nextDirections = new HashSet(Direction.all)
+	this.nextDirections.remove(direction.back)
     }
 
-    @Override
-    void finish(final Graph.DfsStep<Position> step) {
-        if(step.predecessor != null) {
-            Direction toMove = step.vertex.move(step.predecessor)
-            ioBus.readChannel.put(toMove.val)
-            Status response = Status.from(ioBus.writeChannel.take())
-            assert response != Status.WALL
-        }
+    Direction getBack() { direction ? direction.back : null }
+    
+    Tries next(Map<Position, Tries> graph) {
+	def iter = nextDirections.iterator()
+	while(iter.hasNext()) {
+	    Direction dir = iter.next()
+	    Tries tries = new Tries(position, dir)
+	    iter.remove()
+	    if(!graph.containsKey(tries.position)) {
+		return tries
+	    }
+	}
 
-        super.finish(step)
+	return null
     }
 }
 
-def discover() {
-    def origin = new Position(0,0)
+Map<Position,Content> robotDfs(IoBus bus) {
+    Map<Position,Tries> tmp = [:]
+    Position current = new Position(0, 0)
+    tmp[current] = new Tries(current)
+
+    while(true) {
+	Tries currentTries = tmp[current]
+	Tries nextTries = currentTries.next(tmp)
+	if(nextTries == null && !currentTries.back) {
+	    break;
+	}
+	else if(nextTries == null && currentTries.back) {
+	    bus.readChannel.put(currentTries.back.val)
+	    final Content back = Content.from(bus.writeChannel.take())
+	    assert (back == Content.FREE || back == Content.OXYGEN)
+	    current = current + currentTries.back
+	}
+	else {
+	    bus.readChannel.put(nextTries.direction.val)
+	    Content c = Content.from(bus.writeChannel.take())
+	    if(c != Content.WALL) {
+		nextTries.content = c
+		tmp[nextTries.position] = nextTries
+		current = nextTries.position
+	    }
+	}
+    }
+
+    tmp.inject([:]) { map, pos, tries -> map << new MapEntry(pos, tries.content) }
+}
+
+Map discover() {
     def ioBus = IoBus.separateChannels()
     def intCode = Intcode.from(new File("data/15"), ioBus)
-    Graph<Position> graph = new Graph<>([origin])
-    DiscoveringDfsWalk walk = new DiscoveringDfsWalk(graph: graph, ioBus: ioBus)
+    def graph = [:]
     
     def robotThread = Thread.start {
         try {
@@ -113,18 +114,57 @@ def discover() {
     }
 
     def controlThread = Thread.start {
-        graph.depthFirstSearch(walk, origin)
+	graph.putAll(robotDfs(ioBus))
         robotThread.interrupt()
     }
 
     controlThread.join()
     robotThread.join()
 
-    return [ origin: origin, graph: graph, oxygen: walk.oxygen ]
+    return graph
 }
 
-def out = discover()
-def sp = out.graph.dijkstra(out.origin)
-def o2sp = out.graph.dijkstra(out.oxygen)
-def longestShortest = out.graph.nodes.keySet().collect { n -> o2sp.pathTo(n).size() - 1 }.max()
-println "1: ${sp.pathTo(out.oxygen).size() - 1}, 2: ${longestShortest}"
+List dijkstra(Map<Position,Content> graph) {
+    BestCost bc = new BestCost(Heap.min())
+    bc.add(new Position(0,0), 0)
+    Position pos = null
+    while((pos = bc.next()) != null) {
+	int cost = bc.cost(pos)
+	if(graph[pos] == Content.OXYGEN) {
+	    return [pos, cost]
+	}
+
+	Direction.all.each { dir ->
+	    if(graph.containsKey(pos + dir)) {
+		bc.add(pos + dir, cost + 1)
+	    }
+	}
+    }
+}
+
+int spread(Map<Position,Content> graph, Position start) {
+    final Set<Position> oxygens = new HashSet<>()
+    oxygens.add(start)
+    int steps = 0
+    while(oxygens.size() != graph.size()) {
+	Set<Position> toAdd = new HashSet<>()
+	for(Position oxygen : oxygens) {
+	    Direction.all.each { dir ->
+		Position toTry = oxygen + dir
+		if(graph.containsKey(toTry) && !oxygens.contains(toTry)) {
+		    toAdd.add(toTry)
+		}
+	    }
+	}
+
+	oxygens.addAll(toAdd)
+	++steps
+    }
+
+    return steps
+}
+
+def graph = discover()
+def (oxygen, cost) = dijkstra(graph)
+printAssert("Part 1:", cost, 412, "Part 2:", spread(graph, oxygen), 418)
+
