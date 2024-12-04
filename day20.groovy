21e34f1f7866abefcb0e1dc90d80875f7439fa8e
David Clark <me@nobody.com>
2024-12-03 22:35:54 -0600

    Finished day 20

--- day20.groovy
+++ day20.groovy
@@ -10,16 +10,22 @@ enum Kind {
 class Point {
     final int row
     final int col
+    final int level
     final String label
     final Kind kind
 
     Point(int row, int col) {
-	this(row, col, null, null)
+	this(row, col, 0, null, null)
+    }
+
+    Point(int row, int col, int level) {
+	this(row, col, level, null, null)
     }
     
-    Point(int row, int col, String label, Kind kind) {
+    Point(int row, int col, int level, String label, Kind kind) {
 	this.row = row
 	this.col = col
+	this.level = level
 	this.label = label
 
 	if('AA' == label)
@@ -29,6 +35,10 @@ class Point {
 	else
 	    this.kind = kind
     }
+
+    Point changeLevel(int newLevel) {
+	return new Point(row, col, newLevel, label, kind)
+    }
     
     @Override
     boolean equals(Object rhs) {
@@ -36,87 +46,165 @@ class Point {
 	    return false
 
 	Point other = (Point) rhs
-	return row == other.row && col == other.col
+	return row == other.row && col == other.col && level == other.level
     }
 
     @Override
-    int hashCode() { return row + 31 * col }
+    int hashCode() {
+	final int prime = 31
+	int hash = row
+	hash = prime * hash + col
+	hash = prime * hash + level
+	return hash
+    }
 
     @Override
-    String toString() { "(${row},${col},${label},${kind})" }
+    String toString() { "(${row},${col},${level},${label},${kind})" }
 
     List<Point> getPossibleNeighbors() {
-	[ new Point(row,col-1), new Point(row,col+1), new Point(row-1,col), new Point(row+1,col) ]
+	[new Point(row,col-1,level), new Point(row,col+1,level),
+	 new Point(row-1,col,level), new Point(row+1,col,level) ]
+    }
+}
+
+class Maze {
+    final Map<Point,Point> points
+    final Point start
+    final Point finish
+    
+    Maze(Map<Point,Point> points) {
+	this.points = points
+	this.start = points.keySet().find { it.kind == Kind.START }
+	this.finish = points.keySet().find { it.kind == Kind.FINISH }
     }
     
-    List<Point> visitable(Map<Point,Point> maze, Set<Point> visited) {
-	possibleNeighbors.inject([]) { List<Point> list, Point possible ->
-	    if(maze.containsKey(possible) && !visited.contains(possible))
-		return list << maze[possible]
-	    else
-		return list
+    Maze(List<String> lines) {
+	this(parse(lines))
+    }
+
+    static Map<Point,Point> parse(List<String> lines) {
+	def tmp = [:]
+	lines.eachWithIndex { String line, int row ->
+	    line.eachWithIndex { String ch, int col ->
+		if(ch == '.') {
+		    String label = ''
+		    Kind kind = Kind.MID
+		    if(isLetter(line[col-1] as char)) {
+			label = line[col-2] + line[col-1]
+			kind = (col-2 == 0) ? Kind.OUTER : Kind.INNER
+		    }
+		    else if(isLetter(line[col+1] as char)) {
+			label = line[col+1] + line[col+2]
+			kind = (col+3 == line.length()) ? Kind.OUTER : Kind.INNER
+		    }
+		    else if(isLetter(lines[row-1][col] as char)) {
+			label = lines[row-2][col] + lines[row-1][col]
+			kind = (row-2 == 0) ? Kind.OUTER : Kind.INNER
+		    }
+		    else if(isLetter(lines[row+1][col] as char)) {
+			label = lines[row+1][col] + lines[row+2][col]
+			kind = (row+3 == lines.size()) ? Kind.OUTER : Kind.INNER
+		    }
+		    
+		    final Point p = new Point(row, col, 0, label, kind)
+		    tmp[p] = p
+		}
+	    }
+	}
+
+	return Map.copyOf(tmp)
+    }
+
+    Point getAt(Point p) { points[p] }
+
+    Point warpFor(Point point) {
+	points.keySet().find { Point p -> p.label == point.label && p != point }
+    }
+    
+    List<Point> canVisit(Point point) {
+	List<Point> ret = []
+	
+	if(point.kind.warp) {
+	    ret << warpFor(point)
+	}
+	
+	point.possibleNeighbors.inject(ret) { List<Point> list, Point possible ->
+	    this[possible] ? list << this[possible] : list
 	}
     }
 
-    Point warpable(Map<Point,Point> maze, Set<Point> visited) {
-	if(!kind.warp)
-	    return null
-
-	Point possible = maze.keySet().find { Point p -> p.label == label && p != this }
-	return (possible && !visited.contains(possible)) ? possible : null
-    }
-}
-
-class Level {
-    final Set<Point> visited
-    final Point via
-
-    Level(Point via) { this(via, new HashSet()) }
-
-    Level(Point via, Set visited) {
-	this.via = via
-	this.visited = visited
-    }
-    
-    Level copy() { new Level(via, new HashSet(visited)) }
-}
-
-static Map<Point,Point> parseMaze(List<String> lines) {
-    Map<Point,Point> tmp = [:]
-    
-    lines.eachWithIndex { String line, int row ->
-	line.eachWithIndex { String ch, int col ->
-	    if(ch == '.') {
-		String label = ''
-		Kind kind = Kind.MID
-		if(isLetter(line[col-1] as char)) {
-		    label = line[col-2] + line[col-1]
-		    kind = (col-2 == 0) ? Kind.OUTER : Kind.INNER
-		}
-		else if(isLetter(line[col+1] as char)) {
-		    label = line[col+1] + line[col+2]
-		    kind = (col+3 == line.length()) ? Kind.OUTER : Kind.INNER
-		}
-		else if(isLetter(lines[row-1][col] as char)) {
-		    label = lines[row-2][col] + lines[row-1][col]
-		    kind = (row-2 == 0) ? Kind.OUTER : Kind.INNER
-		}
-		else if(isLetter(lines[row+1][col] as char)) {
-		    label = lines[row+1][col] + lines[row+2][col]
-		    kind = (row+3 == lines.size()) ? Kind.OUTER : Kind.INNER
-		}
-
-		final Point p = new Point(row, col, label, kind)
-		tmp[p] = p
+    int solve() {
+	Set<Point> visited = new HashSet()
+	List working = new LinkedList() << [ start, 0 ]
+	
+	while(working) {
+	    def (Point point, int soFar) = working.pop()
+	    if(point == finish)
+		return soFar
+	    
+	    visited.add point
+	    
+	    canVisit(point).each { Point next ->
+		if(!(next in visited))
+		    working << [next, soFar+1]
 	    }
 	}
     }
+}
 
-    return Map.copyOf(tmp)
+class Maze2 extends Maze {
+    Maze2(List<String> lines) {
+	super(levels(parse(lines)))
+    }
+
+    @Override
+    Point warpFor(Point point) {
+	def warp
+	if(point.kind == Kind.INNER) {
+	    warp = points.keySet().find { Point p ->
+		p.label == point.label && p.kind == Kind.OUTER && p.level == (point.level + 1)
+	    }
+	}
+	else {
+	    warp = points.keySet().find { Point p ->
+		p.label == point.label && p.kind == Kind.INNER && p.level == (point.level - 1)
+	    }
+	}
+
+	if(!warp) {
+	    println "Could not find warp for ${point}"
+	    System.exit(0)
+	}
+
+	warp
+    }
+
+    static Map<Point,Point> keepIf(Map<Point,Point> map, int level, Closure closure) {
+	map.inject([:]) { ret, ignore, point ->
+	    Point newPoint = point.changeLevel(level)
+	    closure(point) ? ret << new MapEntry(newPoint, newPoint) : ret
+	}
+    }
+    
+    static Map<Point,Point> levels(Map<Point,Point> map) {
+	Map<Point,Point> accum = [:]
+	//ground level
+	accum += keepIf(map, 0) { point -> point.kind != Kind.OUTER }
+
+	//next levels
+	final Integer total = map.values().sum { p -> p.kind == Kind.INNER ? 1 : 0 }
+	(1..<total).each { level ->
+	    accum += keepIf(map, level) { p -> !(p.kind in [Kind.START, Kind.FINISH]) }
+	}
+
+	//final level
+	accum += keepIf(map, total) { p -> !(p.kind in [Kind.START, Kind.FINISH, Kind.INNER]) }
+	return Map.copyOf(accum)
+    }
 }
 
 static testParse() {
-    def maze = parseMaze(new File('data/20').readLines())
+    def maze = new Maze(new File('data/20').readLines())
     assert maze[new Point(63,2)].kind == Kind.START
     assert maze[new Point(112,59)].kind == Kind.FINISH
 
@@ -138,87 +226,11 @@ static testParse() {
 testParse()
 
 static part1(String path) {
-    Map<Point,Point> maze = parseMaze(new File(path).readLines())
-    Point start = maze.keySet().find { it.kind == Kind.START }
-    Point finish = maze.keySet().find { it.kind == Kind.FINISH }
-
-    Set<Point> visited = new HashSet()
-    List working = new LinkedList()
-    working.add([start, 0])
-    Point point
-    int soFar
-    
-    while(working) {
-	(point, soFar) = working.pop()
-	if(point == finish)
-	    return soFar
-	
-	visited.add point
-	
-	point.visitable(maze, visited).each { Point next ->
-	    working.add([next, soFar+1])
-	}
-
-	Point warp = point.warpable(maze, visited)
-	if(warp)
-	    working.add([warp, soFar+1])
-    }
+    new Maze(new File(path).readLines()).solve()
 }
 
+//println part1('data/20')
 
-static part2(String path) {
-    Map<Point,Point> maze = parseMaze(new File(path).readLines())
-    Map<Point,Point> bottom = Map.copyOf(maze.findAll { k, v -> k.kind != Kind.OUTER })
-    Map<Point,Point> other = Map.copyOf(maze.findAll { k, v -> !(k.kind in [Kind.START, Kind.FINISH]) })
-    Point start = maze.keySet().find { it.kind == Kind.START }
-    Point finish = maze.keySet().find { it.kind == Kind.FINISH }
-    
-    List working = new LinkedList() //next, soFar, recursion, List<Level>
-    working.add([start, 0, 0, [new Level(null)]])
-    Point point
-    int soFar
-    int recursion
-    List<Level> levelStack
-    
-    println "max recursive depth should be: ${maze.keySet().findAll { it.kind == Kind.INNER }.size()}"
-    
-    while(working) {
-	(point, soFar, recursion, levelStack) = working.pop()
-	if(point == finish)
-	    return soFar
-	
-	Level currentLevel = levelStack[recursion]
-	currentLevel.visited.add point
-	Map<Point,Point> currentMaze = (recursion > 0) ? other : bottom
-	
-	point.visitable(currentMaze, currentLevel.visited).each { Point next ->
-	    working.add([next, soFar+1, recursion, levelStack])
-	}
-	
-	if(point.kind == Kind.INNER && !(0..recursion).any { idx -> levelStack[idx].via == point }) {
-	    final int nextRecursion = recursion + 1
-	    final boolean fresh = nextRecursion == levelStack.size()
-	    final Level nextLevel = fresh ? new Level(point) : levelStack[nextRecursion]
-	    final Point warp = point.warpable(other, nextLevel.visited)
-	    if(warp) {
-		if(fresh) {
-		    List<Set> nextStack = levelStack.collect { it.copy() } + nextLevel
-		    working.add([warp, soFar+1, nextRecursion, nextStack])
-		}
-		else {
-		    working.add([warp, soFar+1, nextRecursion, levelStack])
-		}
-	    }
-	}
-	else if(point.kind == Kind.OUTER) {
-	    final int prevRecursion = recursion - 1
-	    final Level prevLevel = levelStack[prevRecursion]
-	    final Map<Point,Point> prevMaze = (recursion > 0) ? other : bottom
-	    final Point warp = point.warpable(prevMaze, prevLevel.visited)
-	    if(warp)
-		working.add([warp, soFar+1, prevRecursion, levelStack])
-	}
-    }
-}
-
-println part2('data/20c')
+def m2 = new Maze2(new File('data/20').readLines())
+println m2.solve()
+//m2.points.each { println it }
